8d976d498ba956882c4e45a0cd05cfdf10461373
Richard Westmoreland <kegtrack@gmail.com>
2021-09-28 12:20:47 -0500

    change task background to red

--- src/components/Task.js
+++ src/components/Task.js
@@ -22,6 +22,7 @@ const Task = ({ task: { id, title, state }, onArchiveTask, onPinTask }) => {
 					value={title}
 					readOnly={true}
 					placeholder='Input title'
+					style={{ background: 'red' }}
 				/>
 			</div>
 
